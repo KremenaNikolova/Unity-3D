76085501a0e4ca2b90ac4701ba64b9efc64cd999
Kremena Nikolova <aradia1992@gmail.com>
2023-05-22 09:58:50 +0300

    Initial commit

--- Basic/02-Collisions-and-Physics-Airplane/Assets/Scripts/ItemDropper.cs
+++ Basic/02-Collisions-and-Physics-Airplane/Assets/Scripts/ItemDropper.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 
 public class ItemDropper : MonoBehaviour
 {
-    public GameObject itemToDrop;
+    public GameObject cube;
+    private Rigidbody rb;
+    private float rbMass = 0.1f;
+
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject item = GameObject.Instantiate(itemToDrop, transform.position, new Quaternion());
-            item.SetActive(true);
+            GameObject clone = Instantiate(cube, cube.transform.position, new Quaternion());
+            clone.SetActive(true);
+
+
+            rb = clone.AddComponent<Rigidbody>();
+            rb.mass = rbMass;
+
+            Destroy(clone, 5);
         }
     }
 }
